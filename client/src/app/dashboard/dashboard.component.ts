33a263c631aecc2b5e324df81d4db1a938533f97
SKPrajapati-dev <skparmar67.sp@gmail.com>
2020-04-19 00:11:36 +0530

    gitignore

--- client/src/app/dashboard/dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'dashboard-root',
-  templateUrl: './dashboard.component.html'
-})
-export class DashboardComponent {
-  
-}
--- server/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  database: 'mongodb+srv://dbUser:Users.DB@mongo@cpcluster-xrgru.mongodb.net/connectProDev?retryWrites=true&w=majority',
-  secret: "coNNropshe5j"
-}
